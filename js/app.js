258bf94ddf7139f28ae0e8bf81f3d1b9d5046ac5
lukas nowy <lukas.n912@gmail.com>
2021-12-21 02:38:53 +0100

    List cars on main page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
 $(document).ready(function() {
-    alert("ready");
-})
+    init();
+});
+
+function init() {
+    $.ajax({
+        type: "get",
+        url: "http://localhost:8080/cars",
+        success: (data) => {
+            console.log(data);
+            listCars(data);
+        },
+        error: (err) => {
+            console.log(err);
+        }
+    });
+}
+
+function listCars(data) {
+    for(item of data) {
+        createCard(item);
+    }
+
+}
+
+function createCard(item) {
+    $(".car_container").append(`
+    <div class="col s12 m6 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/car.jpg">
+          <span class="card-title">${item.brand}</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>${item.model}</p>
+        </div>
+      </div>
+    </div>
+    `);
+}
